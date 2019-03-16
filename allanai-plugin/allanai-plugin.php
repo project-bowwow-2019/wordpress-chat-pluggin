d861c43d206090f341b281a3ea6730d31b10411f
RobChen <robert2030ca@gmail.com>
2019-03-16 18:49:46 -0400

    first commit

---
+++ allanai-plugin/allanai-plugin.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * WP-Reactivate
+ *
+ *
+ * @package  	Allan AI Plugin
+ * @author    Allan AI
+ * @license   GPL-3.0
+ * @link      https://allanai.io
+ * @copyright 2019 Allan AI
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Allan AI Plugin
+ * Plugin URI:        https://allanai.io
+ * Description:       Allan AI chat window plugin
+ * Version:           1.0.0
+ * Author:            Allan AI
+ * Author URI:        https://allanai.io
+ * Text Domain:       allanai-plugin
+ * License:           GPL-3.0
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ */
+
+
+namespace AllanAI\WPR;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'ALLANAI_PLUGIN_VERSION', '1.0.0' );
+
+
+/**
+ * Autoloader
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ *
+ *  * @since 1.0.0
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = __NAMESPACE__;
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/includes/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+/**
+ * Initialize Plugin
+ *
+ * @since 1.0.0
+ */
+function init() {
+	$wpr = Plugin::get_instance();
+	$wpr_shortcode = Shortcode::get_instance();
+	$wpr_admin = Admin::get_instance();
+	$wpr_rest = Endpoint\Example::get_instance();
+}
+add_action( 'plugins_loaded', 'AllanAI\\WPR\\init' );
+
+
+
+/**
+ * Register the widget
+ *
+ * @since 1.0.0
+ */
+function widget_init() {
+	return register_widget( new Widget );
+}
+add_action( 'widgets_init', 'AllanAI\\WPR\\widget_init' );
+
+/**
+ * Register activation and deactivation hooks
+ */
+register_activation_hook( __FILE__, array( 'AllanAI\\WPR\\Plugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'AllanAI\\WPR\\Plugin', 'deactivate' ) );
---
+++ allanai-plugin/includes/Endpoint/Admin.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * WP-Reactivate
+ *
+ *
+ * @package  	Allan AI Plugin
+ * @author    Allan AI
+ * @license   GPL-3.0
+ * @link      https://allanai.io
+ * @copyright 2019 Allan AI
+ */
+
+namespace AllanAI\WPR\Endpoint;
+use AllanAI\WPR;
+
+/**
+ * @subpackage REST_Controller
+ */
+class Admin {
+    /**
+	 * Instance of this class.
+	 *
+	 * @since    0.8.1
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin by setting localization and loading public scripts
+	 * and styles.
+	 *
+	 * @since     0.8.1
+	 */
+	private function __construct() {
+        $plugin = WPR\Plugin::get_instance();
+		$this->plugin_slug = $plugin->get_plugin_slug();
+	}
+
+    /**
+     * Set up WordPress hooks and filters
+     *
+     * @return void
+     */
+    public function do_hooks() {
+        add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+    }
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     0.8.1
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+			self::$instance->do_hooks();
+		}
+
+		return self::$instance;
+	}
+
+    /**
+     * Register the routes for the objects of the controller.
+     */
+    public function register_routes() {
+        $version = '1';
+        $namespace = $this->plugin_slug . '/v' . $version;
+        $endpoint = '/admin/';
+
+        register_rest_route( $namespace, $endpoint, array(
+            array(
+                'methods'               => \WP_REST_Server::READABLE,
+                'callback'              => array( $this, 'get_example' ),
+                'permission_callback'   => array( $this, 'example_permissions_check' ),
+                'args'                  => array(),
+            ),
+        ) );
+
+        register_rest_route( $namespace, $endpoint, array(
+            array(
+                'methods'               => \WP_REST_Server::CREATABLE,
+                'callback'              => array( $this, 'update_example' ),
+                'permission_callback'   => array( $this, 'example_permissions_check' ),
+                'args'                  => array(),
+            ),
+        ) );
+
+        register_rest_route( $namespace, $endpoint, array(
+            array(
+                'methods'               => \WP_REST_Server::EDITABLE,
+                'callback'              => array( $this, 'update_example' ),
+                'permission_callback'   => array( $this, 'example_permissions_check' ),
+                'args'                  => array(),
+            ),
+        ) );
+
+        register_rest_route( $namespace, $endpoint, array(
+            array(
+                'methods'               => \WP_REST_Server::DELETABLE,
+                'callback'              => array( $this, 'delete_example' ),
+                'permission_callback'   => array( $this, 'example_permissions_check' ),
+                'args'                  => array(),
+            ),
+        ) );
+
+    }
+
+    /**
+     * Get Example
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|WP_REST_Request
+     */
+    public function get_example( $request ) {
+        $example_option = get_option( 'allanai_plugin_email' );
+
+        // Don't return false if there is no option
+        if ( ! $example_option ) {
+            return new \WP_REST_Response( array(
+                'success' => true,
+                'value' => ''
+            ), 200 );
+        }
+
+        return new \WP_REST_Response( array(
+            'success' => true,
+            'value' => $example_option
+        ), 200 );
+    }
+
+    /**
+     * Create OR Update Example
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|WP_REST_Request
+     */
+    public function update_example( $request ) {
+        $updated = update_option( 'allanai_plugin_email', $request->get_param( 'exampleSetting' ) );
+
+        return new \WP_REST_Response( array(
+            'success'   => $updated,
+            'value'     => $request->get_param( 'exampleSetting' )
+        ), 200 );
+    }
+
+    /**
+     * Delete Example
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|WP_REST_Request
+     */
+    public function delete_example( $request ) {
+        $deleted = delete_option( 'allanai_plugin_email' );
+
+        return new \WP_REST_Response( array(
+            'success'   => $deleted,
+            'value'     => ''
+        ), 200 );
+    }
+
+    /**
+     * Check if a given request has access to update a setting
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|bool
+     */
+    public function example_permissions_check( $request ) {
+        return current_user_can( 'manage_options' );
+    }
+}
